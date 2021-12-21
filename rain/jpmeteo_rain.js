3d11d36f32f987a29fd6ea4812ee19d5b36e62a0
Jeremy <jeremy.aubert@gmail.com>
2021-12-21 10:01:07 +0100

    ou

--- rain/jpmeteo_rain.js
+++ rain/jpmeteo_rain.js
@@ -1,11 +1,11 @@
 var makeItRain = function() {
     //clear out everything
     $('.rain').empty();
-  
+
     var increment = 0;
     var drops = "";
     var backDrops = "";
-  
+
     while (increment < 100) {
       //couple random numbers to use for various randomizations
       //random number between 98 and 1
@@ -18,14 +18,9 @@ var makeItRain = function() {
       drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
       backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
     }
-  
+
     $('.rain.front-row').append(drops);
     $('.rain.back-row').append(backDrops);
   }
 
-  
-  $('.back-row-toggle.toggle').on('click', function() {
-    $('body').toggleClass('back-row-toggle');
-    $('.back-row-toggle.toggle').toggleClass('active');
-    makeItRain();
-  });
+makeItRain();
